6bf54940c772b21c6888d293f006aef1a646ef24
Michal Drahorat <michaldrahorat@gmail.com>
2018-11-01 15:18:27 -0400

    Add project files.

---
+++ SOCMSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SOCMSite.Startup))]
+namespace SOCMSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
